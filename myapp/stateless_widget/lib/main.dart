695706d289829cfa5dffd2d9b8d8a09754135cb2
Visal Yun <yunvisal18@kit.edu.kh>
2019-11-04 10:43:32 +0700

    Update main.dart

--- myapp/stateless_widget/lib/main.dart
+++ myapp/stateless_widget/lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  
+  home: Home()
 ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://i.pinimg.com/originals/0b/ac/f6/0bacf62a4bd456d02d02c6b8a5c98f67.jpg'),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
